1b480e1c26ad51d5a470b8470e2bc9ed2ddde770
Serap <77828715+serapo@users.noreply.github.com>
2022-06-14 15:48:08 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// function hello(){
+//     console.log('Hello World');
+// }
+// let intervalId=setTimeout(hello,3000);
+// console.log("This message is shown first");
+// console.log("Id :" +intervalId);
+// clearTimeout(intervalId);
+// console.log("setTimeout durduruldu");
+
+// let intervalID=setInterval(hello,000);
+// console.log(intervalID);
+
+
+
+// let myPromise=new Promise(function(resolve,reject){
+//     resolve('Success!');
+//     reject('Failure');
+
+// });
+
+// myPromise.then(
+//     function(value){
+//         console.log(value);
+//     },
+//     function(error){
+//         console.log(error);
+//     }
+// )
